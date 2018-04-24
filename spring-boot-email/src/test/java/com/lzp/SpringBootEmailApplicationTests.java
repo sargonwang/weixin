29df5fe516234398f1e5049f8a17973b7fa3ea1b
刘志鹏(研发部) <liuzp@nsw.com>
2018-04-24 15:55:06 +0800

    create spring-boot-email mudule

---
+++ spring-boot-email/src/test/java/com/lzp/SpringBootEmailApplicationTests.java
@@ -0,0 +1,29 @@
+package com.lzp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootEmailApplicationTests {
+	@Value("${spring.mail.username}")
+	private String userName;
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Test
+	public void sendSimpleEmail(){
+		SimpleMailMessage message=new SimpleMailMessage();
+		message.setFrom(userName);//发送者
+		message.setTo("492782442@qq.com");//接收者
+		message.setSubject("测试主题");//邮件主题
+		message.setText("测试内容");//邮件内容
+		javaMailSender.send(message);
+	}
+}
