3ed796c1cc1c57cd4288742205136369ccff9ed4
刘志鹏(研发部) <liuzp@nsw.com>
2018-05-10 17:30:46 +0800

    添加网站扫码登录模块

--- spring-boot-email/src/test/java/com/lzp/SpringBootEmailApplicationTests.java
+++ spring-boot-email/src/test/java/com/lzp/SpringBootEmailApplicationTests.java
@@ -21,7 +21,7 @@ public class SpringBootEmailApplicationTests {
 	public void sendSimpleEmail(){
 		SimpleMailMessage message=new SimpleMailMessage();
 		message.setFrom(userName);//发送者
-		message.setTo("492782442@qq.com");//接收者
+		message.setTo("2017720063@qq.com");//接收者
 		message.setSubject("测试主题");//邮件主题
 		message.setText("测试内容");//邮件内容
 		javaMailSender.send(message);
