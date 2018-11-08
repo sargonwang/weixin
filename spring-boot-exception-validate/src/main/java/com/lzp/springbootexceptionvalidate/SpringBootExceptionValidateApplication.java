09712238dec9bca6489f507c283ab2fcaeecbb6c
liuzhipeng <492782442@qq.com>
2018-11-08 18:51:29 +0800

    upload validate

---
+++ spring-boot-exception-validate/src/main/java/com/lzp/springbootexceptionvalidate/SpringBootExceptionValidateApplication.java
@@ -0,0 +1,12 @@
+package com.lzp.springbootexceptionvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptionValidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExceptionValidateApplication.class, args);
+    }
+}
---
+++ spring-boot-exception-validate/src/test/java/com/lzp/springbootexceptionvalidate/SpringBootExceptionValidateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lzp.springbootexceptionvalidate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootExceptionValidateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
